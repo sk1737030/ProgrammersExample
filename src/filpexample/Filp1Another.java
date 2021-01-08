43fd733fecf0197274c76bf850d4ce75f03f4ade
sk1737030 <sk1737030@naver.com>
2021-01-08 15:25:26 +0900

    add: 3진수 뒤집기 다른사람 풀이 https://programmers.co.kr/learn/courses/30/lessons/68935/solution_groups?language=java

---
+++ src/filpexample/Filp1Another.java
@@ -0,0 +1,27 @@
+package filpexample;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/68935/solution_groups?language=java
+ */
+public class Filp1Another {
+    public int solution(int n) {
+        int answer = 0;
+        String third = Integer.toString(n, 3);
+        StringBuffer sb = new StringBuffer(third);
+        String reversed = sb.reverse().toString();
+
+        int exp = 0;
+        for (int i = reversed.length() - 1; i >= 0; i--) {
+            answer += Integer.parseInt(String.valueOf(reversed.charAt(i))) * Math.pow(3, exp);
+            exp++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Filp1Another filp1 = new Filp1Another();
+        int n1 = 11;
+        System.out.println(filp1.solution(n1)); // 19
+    }
+}
