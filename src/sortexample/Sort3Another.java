ef7cbe972082c4024da24bd059b21652e258fd66
1234 <sk1737030@naver.com>
2021-01-09 23:16:33 +0900

    나누어 떨어지는 숫자 배열 https://programmers.co.kr/learn/courses/30/lessons/12910

---
+++ src/sortexample/Sort3Another.java
@@ -0,0 +1,18 @@
+package sortexample;
+
+import java.util.Arrays;
+
+public class Sort3Another {
+    public int[] solution(int[] arr, int divisor) {
+        return Arrays.stream(arr).filter(value -> value % divisor == 0).sorted().toArray();
+    }
+
+    public static void main(String[] args) {
+        Sort3Another sort3 = new Sort3Another();
+
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+
+        System.out.println(Arrays.toString(sort3.solution(arr, divisor))); // 5,10
+    }
+}
