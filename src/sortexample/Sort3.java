2115ceb3d05e7be1db8aacdd9359ee77d012a2ff
1234 <sk1737030@naver.com>
2021-01-09 23:16:25 +0900

    나누어 떨어지는 숫자 배열 https://programmers.co.kr/learn/courses/30/lessons/12910

--- src/sortexample/Sort3.java
+++ src/sortexample/Sort3.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 /**
  * 나누어 떨어지는 숫자 배열
- * https://programmers.co.kr/learn/courses/30/lessons/12910
+ * https://programmers.co.kr/learn/courses/30/lessons/12910/solution_groups?language=java
  */
 public class Sort3 {
     public int[] solution(int[] arr, int divisor) {
