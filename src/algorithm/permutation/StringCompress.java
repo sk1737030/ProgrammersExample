5961d1981d5f30ab7e6979fd992cac892dd9f7ff
sk1737030 <sk1737030@naver.com>
2021-02-01 14:07:58 +0900

    문자열 압축 https://programmers.co.kr/learn/courses/30/lessons/60057

---
+++ src/algorithm/permutation/StringCompress.java
@@ -0,0 +1,35 @@
+package algorithm.permutation;
+
+import programmers.step2example.kakaexample.KaKao4;
+
+/**
+ * 문자열 압축 기본
+ * aabbcccd -> 2a2b3cd
+ */
+public class StringCompress {
+    public int solution(String s) {
+        String temp = "";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            count++;
+            if (i == s.length() - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                if (count > 1)
+                    temp += count;
+                temp += s.charAt(i);
+                count = 0;
+            }
+
+        }
+
+        System.out.println(temp);
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        StringCompress tringCompress = new StringCompress();
+        String str = "aabbcd";
+        tringCompress.solution(str);
+    }
+}
