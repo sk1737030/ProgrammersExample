b70f2c9123af99966ae58097c6d9833d71097bd6
dongho <sk1737030@naver.com>
2021-02-22 16:24:38 +0900

    더맵게 추가

--- src/programmers/feedback/더맵게/Solution.java
+++ src/programmers/feedback/더맵게/Solution.java
@@ -1,11 +1,26 @@
 package programmers.feedback.더맵게;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
-    public int solution(int[] scoville, int K) {
+    public int solution(final int[] scoville, final int K) {
         int cnt = 0;
+        PriorityQueue<Integer> scovilleQueue = new PriorityQueue<>();
+
         for (int sc : scoville) {
+            scovilleQueue.offer(sc);
+        }
+        int smallest = -1;
+        while (!scovilleQueue.isEmpty()) {
+            smallest = scovilleQueue.poll();
+            if (scovilleQueue.isEmpty() && smallest < K) {
+                return -1;
+            }
+            if (smallest > K) {
+                return cnt;
+            }
+            scovilleQueue.offer(smallest + (scovilleQueue.poll() * 2));
+            cnt++;
         }
 
         return cnt;
