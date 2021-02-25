47e85c84c0ce351650d25ce1eb3b83ad56bafc21
dongho <sk1737030@naver.com>
2021-02-25 13:02:12 +0900

    구명보트 문제 리팩토링

--- src/programmers/feedback/구명보트/Solution.java
+++ src/programmers/feedback/구명보트/Solution.java
@@ -3,33 +3,23 @@ package programmers.feedback.구명보트;
 import java.util.Arrays;
 
 public class Solution {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        boolean[] booleans = new boolean[people.length];
+    public int solution(final int[] people, final int limit) {
+        int answer = people.length;
+        int endIndex = people.length - 1;
 
         Arrays.sort(people);
 
-        int length = people.length;
-        int heavyPersonIndex = length - 1;
-        for (int i = 0; i < length; i++) {
-            //아직 안태움
-            if (booleans[i]) {
-                continue;
+        int startIndex = 0;
+
+        while (endIndex > startIndex) {
+            // 2명 탔으면
+            if (people[startIndex] + people[endIndex--] > limit) {
+                startIndex--;
+            } else {
+                answer--;
             }
-
-            int lightPerson = people[i];
-            booleans[i] = true;
-
-            // 무게가 limit보다 크면서, index가 뒤에서오는 인덱스보다 작거나같으면서, 방문하지 아직 태우지 않았으면
-            while (i < heavyPersonIndex) {
-                if (booleans[heavyPersonIndex] || lightPerson + people[heavyPersonIndex] > limit)
-                    heavyPersonIndex--;
-                else
-                    break;
-            }
-
-            booleans[heavyPersonIndex] = true;
-            answer += 1;
+//            endIndex--
+//            startIndex++;
         }
 
         return answer;
