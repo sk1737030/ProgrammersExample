d2e0e93014fef6d97ac3ce25a9463ae3c04910ba
1234 <sk1737030@naver.com>
2021-03-01 00:32:39 +0900

    바텀ㅇ럽

--- src/programmers/feedback/정수삼각형/Solution.java
+++ src/programmers/feedback/정수삼각형/Solution.java
@@ -1,23 +1,24 @@
 package programmers.feedback.정수삼각형;
 
-import java.util.Arrays;
-
 public class Solution {
-
-    private int[][] dp;
-
     public int solution(int[][] triangle) {
-        for (int i = 1; i < triangle.length; i++) {
+        // 탑 다운
+        /*for (int i = 1; i < triangle.length; i++) {
             triangle[i][0] = triangle[i - 1][0] + triangle[i][0];
             triangle[i][i] = triangle[i - 1][i - 1] + triangle[i][i];
             for (int j = 1; j < i; j++) {
                 triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
             }
+        }*/
+
+        // 바텀 업
+        for (int i = triangle.length - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = Math.max(triangle[i][j] + triangle[i + 1][j], triangle[i][j] + triangle[i + 1][j + 1]);
+            }
         }
 
 
-        return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
+        return triangle[0][0];
     }
-
-
 }
--- test/programmers/feedback/정수삼각형/SolutionTest.java
+++ test/programmers/feedback/정수삼각형/SolutionTest.java
@@ -9,15 +9,15 @@ class SolutionTest {
     @Test
     void 정수삼각형() {
         Solution solution = new Solution();
-        /*assertEquals(30, solution.solution(new int[][]{
-                {7},
-                {3, 8},
-                {8, 1, 0},
-                {2, 7, 4, 4},
-                {4, 5, 2, 6, 5}
-                //{4, 5, 2, 6, 5, 1}
-        }));*/
         assertEquals(30, solution.solution(new int[][]{
+                {7},            // 0
+                {3, 8},         // 1
+                {8, 1, 0},      // 2
+                {2, 7, 4, 4},   // 3
+                {4, 5, 2, 6, 5} // 4
+                //{4, 5, 2, 6, 5, 1}
+        }));
+        assertEquals(22, solution.solution(new int[][]{
                 {7},
                 {3, 8},
                 {1, 7, 0}
