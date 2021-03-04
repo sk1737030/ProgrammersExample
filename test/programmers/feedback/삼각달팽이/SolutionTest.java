ef6ac3433d1121a841d39b0c992ea57bbb57e86b
dongho <sk1737030@naver.com>
2021-03-04 19:52:26 +0900

    테스트 케이스 수정

--- test/programmers/feedback/삼각달팽이/SolutionTest.java
+++ test/programmers/feedback/삼각달팽이/SolutionTest.java
@@ -2,6 +2,8 @@ package programmers.feedback.삼각달팽이;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -9,8 +11,8 @@ class SolutionTest {
     void 삼각달팽이() {
         Solution solution = new Solution();
         //assertEquals(new int[]{1, 2, 9, 3, 10, 8, 4, 5, 6, 7}, solution.solution(4));
-        assertEquals(new int[]{1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9}, solution.solution(5));
-        assertEquals(new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11}, solution.solution(6));
+        assertEquals(Arrays.toString(new int[]{1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9}), Arrays.toString(solution.solution(5)));
+        assertEquals(Arrays.toString(new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11}), Arrays.toString(solution.solution(6)));
     }
 
 }
