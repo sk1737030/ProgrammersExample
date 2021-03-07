2579a6b83999ead2797271161421f9afba76366a
1234 <sk1737030@naver.com>
2021-03-07 15:34:02 +0900

    builder패턴으로 리팩토링

--- src/programmers/feedback/신규_아이디_추천/Solution.java
+++ src/programmers/feedback/신규_아이디_추천/Solution.java
@@ -5,91 +5,114 @@ import java.util.Locale;
 public class Solution {
     public String solution(String new_id) {
         String answer = "";
+        Charter charter = new Charter(new_id)
+                .makeToLowerCase()
+                .removeBanCharList()
+                .removeConsecutiveDot()
+                .removeTrimDto()
+                .removeCharsIfOverFifteen()
+                .checkLengthIfZeroFillA()
+                .checkLengthIfUnderTwoFillLastChar()
+                .removeTrimDto();
 
-        new_id = makeToLowerCase(new_id);
+        return charter.Id.trim();
+
+
+        /*new_id = makeToLowerCase(new_id);
         new_id = removeBanCharList(new_id);
         new_id = removeConsecutiveDot(new_id);
         new_id = removeTrimDto(new_id);
         new_id = removeCharsIfOverFifteen(new_id);
         new_id = checkLengthIfZeroFillA(new_id);
         new_id = checkLengthIfUnderTwoFillLastChar(new_id);
-        new_id = removeTrimDto(new_id);
-        return new_id.trim();
+        new_id = removeTrimDto(new_id);*/
     }
 
-    private String removeTrimDto(String new_id) {
+    static class Charter {
+        private String Id;
 
-        char[] chars = new_id.toCharArray();
-
-        int index = 0;
-
-        while (true) {
-            if (chars[index] == '.') {
-                chars[index] = ' ';
-            }
-
-            if (chars[index] != '.' && chars[index] != ' ' || index == chars.length - 1) {
-                break;
-            }
-            index++;
+        public Charter(String id) {
+            Id = id;
         }
 
-        index = chars.length - 1;
+        public Charter removeTrimDto() {
+            char[] chars = this.Id.toCharArray();
+            int index = 0;
+            while (true) {
+                if (chars[index] == '.') {
+                    chars[index] = ' ';
+                }
 
-        while (true) {
-            if (chars[index] == '.') {
-                chars[index] = ' ';
+                if (chars[index] != '.' && chars[index] != ' ' || index == chars.length - 1) {
+                    break;
+                }
+                index++;
             }
-            if (chars[index] != '.' && chars[index] != ' ' || 0 == index) {
-                break;
+
+            index = chars.length - 1;
+
+            while (true) {
+                if (chars[index] == '.') {
+                    chars[index] = ' ';
+                }
+                if (chars[index] != '.' && chars[index] != ' ' || 0 == index) {
+                    break;
+                }
+                index--;
             }
-            index--;
+
+            this.Id = new String(chars);
+            return this;
         }
 
 
-        return new String(chars);
-    }
-
-
-    private String checkLengthIfUnderTwoFillLastChar(String new_id) {
-        StringBuilder new_idBuilder = new StringBuilder(new_id);
-        while (new_idBuilder.length() <= 2) {
-            new_idBuilder.append(new_idBuilder.charAt(new_idBuilder.length() - 1));
-        }
-
-        return new_idBuilder.toString();
-    }
-
-    private String removeCharsIfOverFifteen(String new_id) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : new_id.toCharArray()) {
-            if (!Character.isWhitespace(c) && sb.length() < 15) {
-                sb.append(c);
+        private Charter checkLengthIfUnderTwoFillLastChar() {
+            StringBuilder new_idBuilder = new StringBuilder(this.Id);
+            while (new_idBuilder.length() <= 2) {
+                new_idBuilder.append(new_idBuilder.charAt(new_idBuilder.length() - 1));
             }
+            this.Id = new_idBuilder.toString();
+            return this;
         }
-        return sb.toString();
-    }
 
-    private String checkLengthIfZeroFillA(String new_id) {
-        return new_id.length() == 0 ? "a" : new_id;
-    }
-
-    private String removeConsecutiveDot(String new_id) {
-        char[] chars = new_id.toCharArray();
-        for (int i = 1; i < chars.length; i++) {
-            if (chars[i - 1] == '.' && chars[i] == '.') {
-                chars[i - 1] = ' ';
+        private Charter removeCharsIfOverFifteen() {
+            StringBuilder sb = new StringBuilder();
+            for (char c : this.Id.toCharArray()) {
+                if (!Character.isWhitespace(c) && sb.length() < 15) {
+                    sb.append(c);
+                }
             }
+            this.Id = sb.toString();
+            return this;
         }
 
-        return new String(chars);
+        private Charter checkLengthIfZeroFillA() {
+            this.Id = this.Id.length() == 0 ? "a" : this.Id;
+            return this;
+        }
+
+        private Charter removeConsecutiveDot() {
+            char[] chars = this.Id.toCharArray();
+            for (int i = 1; i < chars.length; i++) {
+                if (chars[i - 1] == '.' && chars[i] == '.') {
+                    chars[i - 1] = ' ';
+                }
+            }
+            this.Id = new String(chars);
+            return this;
+        }
+
+        private Charter removeBanCharList() {
+            this.Id = this.Id.replaceAll("[^a-z0-9.\\-_]", "");
+            return this;
+        }
+
+        private Charter makeToLowerCase() {
+            this.Id = this.Id.toLowerCase(Locale.ROOT);
+            return this;
+        }
+
     }
 
-    private String removeBanCharList(String new_id) {
-        return new_id.replaceAll("[^a-z0-9.\\-_]", "");
-    }
 
-    private String makeToLowerCase(String new_id) {
-        return new_id.toLowerCase(Locale.ROOT);
-    }
 }
