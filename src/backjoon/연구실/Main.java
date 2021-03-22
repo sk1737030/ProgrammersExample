159b06910edfcf3965c358d79db9d7034cf575b0
1234 <sk1737030@naver.com>
2021-03-22 22:59:22 +0900

    연구실문제

--- src/backjoon/연구실/Main.java
+++ src/backjoon/연구실/Main.java
@@ -3,10 +3,7 @@ package backjoon.연구실;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * 연구실
@@ -23,10 +20,12 @@ public class Main {
     private void buildWall(int[][] virusMap, int n, int m, int iIndex, int jIndex, int cnt) {
 
         if (cnt == 3) {
-            int[][] spreadVirusMap = spreadVirus(virusMap, n, m);
-            //printed(spreadVirusMap);
-            
+            int[][] cloneMap = Arrays.stream(virusMap).map(int[]::clone).toArray(int[][]::new);
+            int[][] spreadVirusMap = spreadVirus(virusMap, cloneMap, n, m);
+            printed(spreadVirusMap);
             // TODO:: Graph 생성 간선연결하고 dfs 돌리고 count 가져오기
+            //Graph graph = makeGraphAndLines(spreadVirusMap, n, m);
+            //System.out.println(graph.dfs());
             return;
         }
 
@@ -43,11 +42,55 @@ public class Main {
         }
     }
 
-    // 바이러스 퍼트리기
-    private int[][] spreadVirus(int[][] virusMap, int n, int m) {
-        int[][] spreadedMap = Arrays.stream(virusMap).map(int[]::clone).toArray(int[][]::new);
+    // 간선 연결
+    private Graph makeGraphAndLines(int[][] spreadVirusMap, int n, int m) {
+        Graph graph = new Graph(n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (spreadVirusMap[i][j] == 0) {
+                    Graph.Node node = makeAdjacentNode(i, j, graph.nodes);
 
+                    // 4방향 간선 연결
+                    if (j + 1 < m && spreadVirusMap[i][j + 1] == 0) {
+                        Graph.Node adjacentNode = makeAdjacentNode(i, j + 1, graph.nodes);
+                        graph.addEdge(node, adjacentNode);
+                    }
+
+                    if (j - 1 >= 0 && spreadVirusMap[i][j - 1] == 0) {
+                        Graph.Node adjacentNode = makeAdjacentNode(i, j - 1, graph.nodes);
+                        graph.addEdge(node, adjacentNode);
+                    }
+
+                    if (i + 1 < n && spreadVirusMap[i + 1][j] == 0) {
+                        Graph.Node adjacentNode = makeAdjacentNode(i + 1, j, graph.nodes);
+                        graph.addEdge(node, adjacentNode);
+                    }
+
+                    if (i - 1 >= 0 && spreadVirusMap[i - 1][j] == 0) {
+                        Graph.Node adjacentNode = makeAdjacentNode(i - 1, j, graph.nodes);
+                        graph.addEdge(node, adjacentNode);
+                    }
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    // 인근 노드 가져오며서 null 이면 새로만들기
+    private Graph.Node makeAdjacentNode(int i, int j, Graph.Node[][] node1) {
+        Graph.Node adjacentNode = node1[i][j];
+        if (adjacentNode == null) {
+            adjacentNode = new Graph.Node(i, j, 0);
+            node1[i][j] = adjacentNode;
+        }
+        return adjacentNode;
+    }
+
+    // 바이러스 퍼트리기
+    private int[][] spreadVirus(int[][] virusMap, int[][] spreadedMap, int n, int m) {
         // TODO:: 안에 4방향 퍼트리는 부분을 줄여보기 재귀로 줄여볼수있을 듯?
+        // TODO:: 퍼뜨린 방향으로 계속 나아가기
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (spreadedMap[i][j] == 2) {
@@ -65,7 +108,6 @@ public class Main {
                     if (i - 1 >= 0 && virusMap[i - 1][j] != 1) {
                         spreadedMap[i - 1][j] = 2;
                     }
-
                 }
             }
         }
@@ -84,7 +126,7 @@ public class Main {
     }
 
     static class Graph {
-        Node[][] node;
+        Node[][] nodes;
 
         static class Node {
             int i;
@@ -101,6 +143,10 @@ public class Main {
             }
         }
 
+        public Graph(int n, int m) {
+            this.nodes = new Node[n][m];
+        }
+
         public void addEdge(Node node, Node node2) {
             if (!node.adjacent.contains(node2)) {
                 node.adjacent.add(node2);
@@ -110,10 +156,38 @@ public class Main {
             }
         }
 
-        public void dfs() {
-            dfs();
+        public int dfs() {
+            int max = 0;
+            for (Node[] nodes : nodes) {
+                for (Node value : nodes) {
+                    if (value != null && !value.visited) {
+                        max = Math.max(max, dfs(value));
+                    }
+                }
+            }
+            return max;
         }
 
+        private int dfs(Node node) {
+            Queue<Node> nodeQueue = new LinkedList<>();
+            nodeQueue.offer(node);
+            node.visited = true;
+
+            int cnt = 1;
+
+            while (!nodeQueue.isEmpty()) {
+                Node poll = nodeQueue.poll();
+
+                for (Node node1 : poll.adjacent) {
+                    if (!node1.visited) {
+                        nodeQueue.offer(node1);
+                        node1.visited = true;
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
+        }
     }
 
     public static void main(String[] args) throws IOException {
--- test/backjoon/연구실/MainTest.java
+++ test/backjoon/연구실/MainTest.java
@@ -8,7 +8,7 @@ class MainTest {
     @Test
     void 연구실() {
         Main main = new Main();
-        /*assertEquals(27, main.solution(7, 7, new int[][]{
+  /*      assertEquals(27, main.solution(7, 7, new int[][]{
                 {2, 0, 0, 0, 1, 1, 0},
                 {0, 0, 1, 0, 1, 2, 0},
                 {0, 1, 1, 0, 1, 0, 0},
@@ -17,10 +17,10 @@ class MainTest {
                 {0, 1, 0, 0, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0, 0}
         }));*/
-        assertEquals(27, main.solution(2, 3, new int[][]{
+        assertEquals(27, main.solution(3, 3, new int[][]{
                 {2, 0, 0},
-                {0, 0, 2}//,
-                //{0, 0, 1},
+                {0, 0, 2},
+                {0, 0, 0}
         }));
     }
 }
