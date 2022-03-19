6d8cc18130feb3244ee5d64b56310a24e5b68849
dongle <sk1737030@naver.com>
2022-03-19 15:27:32 +0900

    fix: time complex 해결

--- src/codility/missinginteger/Solution.java
+++ src/codility/missinginteger/Solution.java
@@ -1,42 +1,23 @@
 package codility.missinginteger;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Solution {
 
     public int solution(int[] A) {
+        // 2,3,4,5  5
+        boolean[] a = new boolean[A.length + 1];
 
-        List<Integer> collect = Arrays.stream(A).boxed()
-            .filter(integer -> integer >= 0)
-            .collect(Collectors.toList());
-
-        Collections.sort(collect);
-
-        if (collect.isEmpty() || collect.get(0) > 1) {
-            return 1;
-        }
-
-        if (collect.size() == 1 && collect.get(0) == 1) {
-            return 2;
-        }
-
-        Integer before = collect.get(0);
-
-        for (Integer integer : collect) {
-            if (before == integer) {
-                continue;
+        for (int i1 : A) {
+            if (i1 > 0 && i1 < a.length) {
+                a[i1] = true;
             }
-
-            if (before + 1 != integer) {
-                return before + 1;
-            }
-
-            before = integer;
         }
 
-        return collect.get(collect.size() - 1) + 1;
+        for (int i = 1; i < a.length; i++) {
+            if (!a[i]) {
+                return i;
+            }
+        }
+
+        return A.length + 1;
     }
 }
