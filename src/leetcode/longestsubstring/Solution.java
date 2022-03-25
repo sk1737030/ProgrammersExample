e6eba0db0164f4d6a51e022a812195298b69336e
dongle <sk1737030@naver.com>
2022-03-25 23:48:45 +0900

    init: leetcode longest substring without repeating characters https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ src/leetcode/longestsubstring/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.longestsubstring;
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        
+        return null;
+    }
+}
---
+++ test/leetcode/longestsubstring/SolutionTest.java
@@ -0,0 +1,7 @@
+package leetcode.longestsubstring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+}
