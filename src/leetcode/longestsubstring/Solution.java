14b7fbb964264117474b66fd3ff3b3d36307d5f1
dongle <sk1737030@naver.com>
2022-03-26 00:57:05 +0900

    feat: time out ˆ

--- src/leetcode/longestsubstring/Solution.java
+++ src/leetcode/longestsubstring/Solution.java
@@ -6,7 +6,35 @@ package leetcode.longestsubstring;
 public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
-        
-        return null;
+        int result = s.length() - 1;
+
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = result; i > 0; i--) {
+            int j = 0;
+
+            do {
+                String substring = s.substring(j, i + j + 1);
+
+                boolean duple = false;
+                for (int i1 = 0; i1 < substring.length(); i1++) {
+                    if (substring.indexOf(substring.charAt(i1)) != i1) {
+                        duple = true;
+                        break;
+                    }
+                }
+
+                if (!duple) {
+                    return substring.length();
+                }
+
+                j++;
+
+            } while (i + j != s.length());
+        }
+
+        return 1;
     }
 }
--- test/leetcode/longestsubstring/SolutionTest.java
+++ test/leetcode/longestsubstring/SolutionTest.java
@@ -1,7 +1,26 @@
 package leetcode.longestsubstring;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
+    @Test
+    void test() {
+
+        Solution solution = new Solution();
+        int result1 = solution.lengthOfLongestSubstring("abcabcbb");
+        assertEquals(3, result1);
+        int result2 = solution.lengthOfLongestSubstring("bbbbb");
+        assertEquals(1, result2);
+        int result3 = solution.lengthOfLongestSubstring("pwwkew");
+        assertEquals(3, result3);
+        int result4 = solution.lengthOfLongestSubstring(" ");
+        assertEquals(1, result4);
+        int result5 = solution.lengthOfLongestSubstring("au");
+        assertEquals(2, result5);
+
+
+    }
 }
