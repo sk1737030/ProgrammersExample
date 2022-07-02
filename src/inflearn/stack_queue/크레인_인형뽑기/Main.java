c4cccb9fc2ea4862fdc45657bcec93ff581868e0
dongle <sk1737030@naver.com>
2022-07-02 18:25:31 +0900

    feat: 크레인인형뽑기

---
+++ src/inflearn/stack_queue/크레인_인형뽑기/Main.java
@@ -0,0 +1,72 @@
+package inflearn.stack_queue.크레인_인형뽑기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int move : moves) {
+            int doll = getDoll(move, board);
+            if (doll != 0) {
+                stack.push(doll);
+            }
+        }
+
+        int beforeDoll = 0;
+
+        for (int i = 0; i < stack.size(); i++) {
+            Integer doll = stack.get(i);
+
+            if (doll == beforeDoll) {
+                stack.remove(i);
+                stack.remove(i - 1);
+                i -= 3;
+                answer += 2;
+            }
+            beforeDoll = doll;
+
+        }
+
+        return answer;
+    }
+
+    private int getDoll(int move, int[][] board) {
+        int length = board.length;
+        int j = move - 1;
+
+        for (int i = 0; i < length; i++) {
+            int doll = board[i][j];
+
+            if (doll != 0) {
+                board[i][j] = 0;
+                return doll;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int M = sc.nextInt();
+        int[] moves = new int[M];
+        for (int i = 0; i < M; i++) {
+            moves[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(board, moves));
+    }
+
+}
---
+++ test/inflearn/stack_queue/크레인_인형뽑기/MainTest.java
@@ -0,0 +1,23 @@
+package inflearn.stack_queue.크레인_인형뽑기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Main T = new Main();
+        int[][] board = {{0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 3},
+            {0, 2, 5, 0, 1},
+            {4, 2, 4, 4, 2},
+            {3, 5, 1, 3, 1}
+        };
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+
+        assertEquals(4, T.solution(board, moves));
+    }
+
+}
