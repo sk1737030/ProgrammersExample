dc56553bfdddbb4f72fd862e23f78a6c6eee3923
dongle <sk1737030@naver.com>
2022-07-10 23:46:46 +0900

    init: 쇠막대기

---
+++ src/main/java/inflearn/stack_queue/쇠막대기/Main.java
@@ -0,0 +1,22 @@
+package inflearn.stack_queue.쇠막대기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(String arg) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String arg = sc.nextLine();
+        int t = T.solution(arg);
+        System.out.println(t);
+    }
+
+
+}
---
+++ src/test/java/inflearn/stack_queue/쇠막대기/MainTest.java
@@ -0,0 +1,15 @@
+package inflearn.stack_queue.쇠막대기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Main t = new Main();
+        assertEquals(17, t.solution("()(((()())(())()))(())"));
+    }
+
+}
