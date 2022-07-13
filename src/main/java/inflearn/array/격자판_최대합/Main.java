4962f4d81a712abb842257ce1a746a6dbb3ff812
dongle <sk1737030@naver.com>
2022-07-13 23:21:43 +0900

    feat: 격자판_최대합

--- src/main/java/inflearn/array/격자판_최대합/Main.java
+++ src/main/java/inflearn/array/격자판_최대합/Main.java
@@ -2,12 +2,45 @@ package inflearn.array.격자판_최대합;
 
 import java.util.Scanner;
 
+/**
+ * 다시 풀기 손도못댐
+ */
 public class Main {
 
     public int solution(int[][] board) {
+        int n = board.length;
+        int answer = Integer.MIN_VALUE;
+        int sum1 = 0;
+        int sum2 = 0;
 
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
 
-        return 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += board[i][j];
+                sum2 += board[j][i];
+            }
+
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = 0;
+        sum2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum1 += board[i][i];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            sum2 += board[i][i];
+        }
+
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
     }
 
     public static void main(String[] args) {
